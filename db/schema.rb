a8058c7c374a7f40af57c38d1eaaf8dc86605fa4
Jhosep <jose-zromero@hotmail.com>
2023-01-26 13:14:42 -0600

    Add foreign key post to comment

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_26_190756) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_26_191218) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -19,7 +19,9 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_26_190756) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "author_id", null: false
+    t.bigint "posts_id", null: false
     t.index ["author_id"], name: "index_comments_on_author_id"
+    t.index ["posts_id"], name: "index_comments_on_posts_id"
   end
 
   create_table "likes", force: :cascade do |t|
@@ -45,5 +47,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_26_190756) do
     t.integer "PostsCounter"
   end
 
+  add_foreign_key "comments", "posts", column: "posts_id"
   add_foreign_key "comments", "users", column: "author_id"
 end
