3701a173b2d5c1c60ce787586b6e7d5bf8b8eb93
Jhosep <jose-zromero@hotmail.com>
2023-01-26 13:11:29 -0600

    Add tables to the squema

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,40 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 0) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_26_190756) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "comments", force: :cascade do |t|
+    t.text "Text"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "author_id", null: false
+    t.index ["author_id"], name: "index_comments_on_author_id"
+  end
+
+  create_table "likes", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.string "Title"
+    t.text "Text"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "CommentsCounter"
+    t.integer "LikesCounter"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "Name"
+    t.string "Photo"
+    t.text "Bio"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "PostsCounter"
+  end
+
+  add_foreign_key "comments", "users", column: "author_id"
 end
