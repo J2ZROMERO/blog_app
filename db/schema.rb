db9adbe382324c98b6b757157a237d98f64e2a27
Jhosep <jose-zromero@hotmail.com>
2023-01-26 07:41:49 -0600

    Add tables to db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,47 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_25_215930) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_26_025440) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.integer "AuthorId"
+    t.integer "PostId"
+    t.text "Text"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["AuthorId"], name: "index_comments_on_AuthorId"
+    t.index ["PostId"], name: "index_comments_on_PostId"
+  end
+
+  create_table "likes", force: :cascade do |t|
+    t.integer "AuthorId"
+    t.integer "PostId"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["AuthorId"], name: "index_likes_on_AuthorId"
+    t.index ["PostId"], name: "index_likes_on_PostId"
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.integer "AuthorId"
+    t.string "Title"
+    t.text "Text"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "CommentsCounter"
+    t.integer "LikesCounter"
+    t.index ["AuthorId"], name: "index_posts_on_AuthorId"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "Name"
+    t.string "Photo"
+    t.text "Bio"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "PostsCounter"
+  end
+
 end
