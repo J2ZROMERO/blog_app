16630bf902e9eabfe937f84f57f7d4b909791baf
Jhosep <jose-zromero@hotmail.com>
2023-01-30 16:38:47 -0600

    Apply rubocop

--- app/controllers/post_controller.rb
+++ app/controllers/post_controller.rb
@@ -1,9 +1,7 @@
 class PostController < ApplicationController
+  def index; end
 
-    def index
-    end
-   
-    def show
-        @post = Post.new(id: params[:id]) #object no proper of the database we use new instead of find
-    end
+  def show
+    @post = Post.new(id: params[:id]) # object no proper of the database we use new instead of find
+  end
 end
