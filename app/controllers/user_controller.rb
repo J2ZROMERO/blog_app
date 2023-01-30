d7f10095359271dfe9e78c67d158ea868345fa61
Jhosep <jose-zromero@hotmail.com>
2023-01-30 12:32:43 -0600

    Add index | assets,css,route,view,index,controller

---
+++ app/controllers/user_controller.rb
@@ -0,0 +1,6 @@
+class UserController < ApplicationController
+  
+    def index
+    end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  resources :create_users
+  root to: 'user#index'
+  resources :user
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
