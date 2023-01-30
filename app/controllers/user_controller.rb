2510362fda477af755abf3e15b4ea5b6e016cc39
Jhosep <jose-zromero@hotmail.com>
2023-01-30 14:23:47 -0600

    Add show | assets,css,route,view,index,controller

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -3,4 +3,8 @@ class UserController < ApplicationController
     def index
     end
 
+    def show
+        @user = User.new(id: params[:id]) #object no proper of the database we use new instead of find
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   root to: 'user#index'
-  resources :user
+  get 'user/:id', to: 'user#show'
+  get 'user/:id', to: 'user#show' do 
+  get 'post', to: 'post#index'
+end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
