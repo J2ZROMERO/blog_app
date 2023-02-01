eeb7f41fd29a0635f7600adc374033d5ecad5580
Jhosep <jose-zromero@hotmail.com>
2023-02-01 13:00:51 -0600

    Add content to the template file, style's path and refactor style's name updated

--- app/controllers/post_controller.rb
+++ app/controllers/post_controller.rb
@@ -1,7 +1,11 @@
 class PostController < ApplicationController
-  def index; end
+  layout 'application'
+  def index
+    @stylesheet = 'post/post'
+  end
 
   def show
+    @stylesheet = 'post/show'
     @post = Post.new(id: params[:id]) # object no proper of the database we use new instead of find
   end
 end
--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,7 +1,12 @@
 class UserController < ApplicationController
-  def index; end
-
+  layout 'application'
+  def index
+     @stylesheet = 'user/user'
+  end
+  
   def show
+    @stylesheet = 'user/show'
     @user = User.new(id: params[:id]) # object no proper of the database we use new instead of find
   end
+  
 end
