7f2a12cfd0e25dd6995440bbdc52008ec0f9914e
Jhosep <jose-zromero@hotmail.com>
2023-02-02 23:53:57 -0600

    Update seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 def add_data
-    first_user = User.create(Name: 'Tom', Photo: 'https://unsplash.com/photos/F_-0BxGuVvo', Bio: lorem 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae possimus sint facere 
+    first_user = User.create(Name: 'Tom', Photo: 'https://unsplash.com/photos/F_-0BxGuVvo', Bio: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae possimus sint facere 
     neque alias repellat asperiores quos eos commodi quibusdam et dolores placeat atque eius, ad excepturi corrupti labore fugiat!.',PostsCounter: 0)
     second_user = User.create(Name: 'Lilly', Photo: 'https://unsplash.com/photos/F_-0BxGuVvo', Bio: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae possimus sint facere neque 
     alias repellat asperiores quos eos commodi quibusdam et dolores placeat atque eius, ad excepturi corrupti labore fugiat!.',PostsCounter: 0)
@@ -16,15 +16,15 @@ def add_data
     first_post = Post.create(author_id: first_user.id, Title: 'Hello', Text:  'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae possimus sint facere neque alias 
     repellat asperiores quos eos commodi quibusdam et dolores placeat atque eius, ad excepturi corrupti labore fugiat!', LikesCounter: 1, CommentsCounter: 0)
     second_post = Post.create(author_id: second_user.id, Title: 'Cats', Text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae possimus sint facere neque 
-    alias repellat asperiores quos eos commodi quibusdam et dolores placeat atque eius, ad excepturi corrupti labore fugiat!, LikesCounter: 9, CommentsCounter: 0)
+    alias repellat asperiores quos eos commodi quibusdam et dolores placeat atque eius, ad excepturi corrupti labore fugiat!', LikesCounter: 9, CommentsCounter: 0)
     third_post = Post.create(author_id: second_user.id, Title: 'Birds', Text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae possimus sint facere neque 
     alias repellat asperiores quos eos commodi quibusdam et dolores placeat atque eius, ad excepturi corrupti labore fugiat!', LikesCounter: 3, CommentsCounter: 0)
     four_post = Post.create(author_id: first_user.id, Title: 'Bear', Text:    'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae possimus sint facere neque 
-    alias repellat asperiores quos eos commodi quibusdam et dolores placeat atque eius, ad excepturi corrupti labore fugiat!, LikesCounter: 8, CommentsCounter: 0)
+    alias repellat asperiores quos eos commodi quibusdam et dolores placeat atque eius, ad excepturi corrupti labore fugiat!', LikesCounter: 8, CommentsCounter: 0)
     five_post = Post.create(author_id: first_user.id, Title: 'Bear', Text:    'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae possimus sint facere neque 
-    alias repellat asperiores quos eos commodi quibusdam et dolores placeat atque eius, ad excepturi corrupti labore fugiat!, LikesCounter: 5, CommentsCounter: 0)
+    alias repellat asperiores quos eos commodi quibusdam et dolores placeat atque eius, ad excepturi corrupti labore fugiat!', LikesCounter: 5, CommentsCounter: 0)
     six_post = Post.create(author_id: first_user.id, Title: 'Bear', Text:     'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae possimus sint facere neque 
-    alias repellat asperiores quos eos commodi quibusdam et dolores placeat atque eius, ad excepturi corrupti labore fugiat!, LikesCounter: 10, CommentsCounter: 0)
+    alias repellat asperiores quos eos commodi quibusdam et dolores placeat atque eius, ad excepturi corrupti labore fugiat!', LikesCounter: 10, CommentsCounter: 0)
     seven_post = Post.create(author_id: first_user.id, Title: 'Bear', Text:   'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae possimus sint facere neque     
     alias repellat asperiores quos eos commodi quibusdam et dolores placeat atque eius, ad excepturi corrupti labore fugiat!', LikesCounter: 13, CommentsCounter: 0)
 
