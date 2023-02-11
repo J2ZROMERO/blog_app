bda16e2bdee0da412afab3246cb81b130786e7d8
AlaaShaheen2 <alaa.m.shaheen22@gmail.com>
2023-02-11 06:10:08 +0200

    added a comment controller to api

---
+++ app/controllers/api/comment_controller.rb
@@ -0,0 +1,9 @@
+class Api::CommentController < ApplicationController
+    
+    def index
+        @comments = Comment.where(author_id: params[:user_id])
+        render json: @comments
+      end
+
+    
+end
